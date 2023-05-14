56f2697dcfa50cfff4262ee6c4f3112b9eeacddb
Brito Reji <britoreji2006@gmail.com>
2023-05-14 10:59:52 +0530

    day first and second

---
+++ assignmentDay2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{   //get sum of numbers that user inpt in console
+    // int Number1;
+    // float Number2,sum=0;
+    // scanf("%d %f",&Number1,&Number2);
+    // sum = Number1+Number2;
+    // printf("sum is:%d",sum);
+    
+
+    //a program to find the simple interest.
+    // int P;
+    // float R,n,SI;
+    // printf("Type the Principal amount");
+    // scanf("%d",&P);
+    // printf("Enter the Interest rate");
+    // scanf("%f",&R);
+    // printf("Number of years");
+    // scanf("%f",&n);
+    // SI = ((P*R*n)/100);
+    // printf("Simple interest is :%f",SI);
+
+
+
+
+    
+    
+
+    return 0;
+}
+
---
+++ assignmentDay3.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+// 1.    a program to check whether a student has passed or failed in a subject after he or
+// she enters their mark (pass mark for a subject is 50 out of 100).
+
+    // float mark;
+    // printf("Enter the mark:");
+    // scanf("%f",&mark);
+    // if (mark>=50 && mark<=100)
+    // {
+    //     printf("Passed");
+    // }
+    // else{
+    //     printf("failed");
+    // }
+
+    //2. A program to show the grade obtained by a student after he/she enters their total mark percentage.
+    // int percentage;
+    // printf("Enter the percentage");
+    // scanf("%d",&percentage);
+    // if (percentage>=90 && percentage<100)
+    // {
+    //     printf("Grade: A");
+
+    // }else if (percentage>90 || percentage>80)
+    // {
+    //     printf("Grade: B");
+    // }else if (percentage>80 || percentage>70)
+    // {
+    //     printf("Grade: C");
+    // }else if (percentage>70 || percentage>60)
+    // {
+    //      printf("Grade: D");
+    // }else if (percentage>60  || percentage>50)
+    // {
+    //     printf("Grade: E");
+    // }else
+    // {
+    //     printf("failed");
+
+    // }
+
+    // 3 a program to accept an input number from the user and output the day as follows.
+
+    // int inp=0;
+    // printf("enter the number of day");
+    // scanf("%d",&inp);
+    // switch (inp)
+    // {
+    // case 1:
+    //     printf("Sunday");
+    //     break;
+    // case 2:
+    //     printf("Monday");
+    //     break;
+    // case 3:
+    //     printf("Tuesday");
+    //     break;
+    // case 4:
+    //     printf("Wednesday");
+    //     break;
+    // case 5:
+    //     printf("Thursday");
+    //     break;
+    // case 6:
+    //     printf("Friday");
+    // case 7:
+    //     printf("Saturday");
+    
+    // default:
+    //     printf("invlid Entry");
+    //     break;
+    // }
+    
+    
+    
+    
+
+    return 0;
+}
