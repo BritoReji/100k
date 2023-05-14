d36b9ed8bfa6b2cc7e9759ea93a3228a40ddeaaf
Brito Reji <britoreji2006@gmail.com>
2023-05-14 11:27:52 +0530

    Day 4

---
+++ assignmentDay4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{// 1) a program to print the multiplication table of given number
+    // int inp,i=0,total;
+    // printf("Enter a number");
+    // scanf("%d",&inp);
+    // for ( i = 1; i <= 10; i++)
+    // {
+    //    total = i*inp;
+    //    printf("%d X %d = %d\n",i,inp,total);
+    // }
+//  2) a program to find the sum of all the odd numbers for a given limit
+    // int limit,i,sum=0;
+    // printf("Enter a limit");
+    // scanf("%d",&limit);
+    // for ( i = 1; i < limit; i++)
+    // {
+    //    if (i%2==1)
+    //    {
+    //     sum+=i;
+    //    }
+       
+    // }
+    // printf("Sum of odd numbers = %d",sum);
+// 3)a program to print the following pattern
+    // 1
+    // 12
+    // 123
+    // 1234
+    // 12345
+
+    // int i,limit=5,j;
+    // for ( i = 1; i <= limit; i++)
+    // {
+    //    for ( j = 1; j <= i; j++)
+    //    {
+    //     printf("%d",j);
+    //    }
+    //    printf("\n");
+    // }
+    
+    
+    return 0;
+}
